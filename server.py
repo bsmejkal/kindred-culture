f5838779802bd529752154d62d7945ff40bfb0c2
Briana Smejkal <brianasmejkal@gmail.com>
2020-03-04 20:25:32 +0000

    Added routes in server

---
+++ server.py
@@ -0,0 +1,42 @@
+""""""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash, session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import Tribe, Location, connect_to_db, db 
+
+
+app = Flask(__name__)
+
+app.secret_key = "SUNDERED"
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+	"""Splash page"""
+
+	return render_template("")
+
+
+@app.route('/about')
+def about():
+	"""About project"""
+
+	return render_template("")
+
+
+if __name__ == "__main__"
+
+	app.debug = True
+	app.jinja_env.auto_reload = app.debug
+
+	connect_to_db(app)
+
+	DebugToolbarExtension(app)
+
+	app.run(port=5000, host='0.0.0.0')
+	
