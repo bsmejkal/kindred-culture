d2d809af6d99dcf7ad2980bc8e2344092e41ec56
Briana Smejkal <brianasmejkal@gmail.com>
2020-03-11 18:49:53 +0000

    Created a json reader function in seed.py

--- seed.py
+++ seed.py
@@ -3,19 +3,29 @@
 from sqlalchemy import func
 from models import Tribes, Locations, connect_to_db, db 
 from routes import app
+import json
+
+
+def json_reader(file_path):
+	"""Opens & loads json files"""
+	
+	with open(file_path) as file:
+		json_dict = json.load(file)
+
+	return json_dict
 
 
 def load_tribes():
-	"""Load Tribe info from indigenousTerritories.json into database."""
+	"""Load Tribe info from indigenousTerritories.json into 
+	database."""
 
-	print('Tribes')
+	# print('Tribes')
 
 	# Tribes.query.delete()
 
-	file = open("seed_data/indigenousTerritories.json")
-	b_str = file.buffer.read()
-	lines = b_str.decode()
-	return lines
-	# for tribe in tribes:
-	# 	name = tribes["features"]["properties"]["Name"]
-	# 	print(name)
+	tribe_file = "seed_data/indigenousTerritories.json"
+
+	tribe_dict = json_reader(tribe_file)
+
+	return tribe_dict
+	
