117845800ac0fc9dbff5fcc01ebaa14921768829
Briana Smejkal <brianasmejkal@gmail.com>
2020-03-11 21:17:26 +0000

    Completed load_tribes stub in seed.py

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-""""""
+"""Utility file to seed kindred database from Native-Land data"""
 
 from sqlalchemy import func
 from models import Tribes, Locations, connect_to_db, db 
@@ -8,7 +8,7 @@ import json
 
 def json_reader(file_path):
 	"""Opens & loads json files"""
-	
+
 	with open(file_path) as file:
 		json_dict = json.load(file)
 
@@ -19,13 +19,43 @@ def load_tribes():
 	"""Load Tribe info from indigenousTerritories.json into 
 	database."""
 
-	# print('Tribes')
+	print('Tribes')
 
-	# Tribes.query.delete()
+	Tribes.query.delete()
 
 	tribe_file = "seed_data/indigenousTerritories.json"
-
 	tribe_dict = json_reader(tribe_file)
 
-	return tribe_dict
+	i = 0
+
+	for key in tribe_dict['features']:
+		name = tribe_dict['features'][i]['properties']['Name']
+		i += 1
+
+		tribe = Tribes(name=name)
+
+		db.session.add(tribe)
+
+	db.session.commit()
 	
+
+def set_val_tribe_id():
+	"""Set value for the next tribe_id after seeding database"""
+
+	result = db.session.query(func.max(Tribes.tribe_id)).one()
+	max_id = int(result[0])
+
+	query = "SELECT setval('tribes_tribe_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+    
+    # Import different types of data
+    load_tribes()
+    set_val_user_id()
