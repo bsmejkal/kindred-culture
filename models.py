02ce6808de281e4f40dc8969c4a02d905fddc24c
Briana Smejkal <brianasmejkal@gmail.com>
2020-03-15 14:48:12 +0000

    Updated seed.py

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+app = Flask(__name__)
+app.secret_key = 'SUNDERED'
 
 class Tribe(db.Model):
 	"""Indigeneous peoples in North America"""
@@ -12,14 +15,12 @@ class Tribe(db.Model):
 	tribe_id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
 	name = db.Column(db.String(), nullable=True, unique=True)
 	region = db.Column(db.String(), nullable=True)
-	language_id = db.relationship('Language', backref='languages')
-	# language = db.Column(db.String(), nullable=True)
-	# language_family = db.Column(db.String(), nullable=True)
-	# location = db.relationship("Locations", backref="tribes")
-
+	description = db.Column(db.String(), nullable=True)
+	language_id = db.Column(db.Integer(), db.ForeignKey('languages.language_id'))
+	
 	def __repr__(self):
 
-		return f"<Tribe id = {tribe_id}, name = {name}>"
+		return f'<Tribe id = {tribe_id}, name = {name}>'
 
 
 class Language(db.Model):
@@ -28,33 +29,21 @@ class Language(db.Model):
 	__tablename__ = 'languages'
 
 	language_id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-	language = db.Column(db.String(), nullable=True, unique=True)
+	language_name = db.Column(db.String(), nullable=True, unique=True)
 	language_family = db.Column(db.String(), nullable=True)
+
 
 	def __repr__(self):
 
-		return f"<Language id = {language_id}, language = {language}>"
-
-
-# class Locations(db.Model):
-# 	"""Location in latitude and longitude"""
-
-# 	__tablename__ = "locations"
-
-# 	location_id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-# 	coordinates = db.Column(db.Integer(), nullable=False)
-# 	region = db.Column(db.String(), nullable=True)
-
-# 	def __repr__(self):
-
-# 		return f"<Location id = {location_id} region = {region}>"
-
+		return f'<Language id = {language_id}, language = {language}>'
 
 
 def connect_to_db(app):
 
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tribes'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///kindred'
+	app.config['SQLALCHEMY_ECHO'] = True
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 	db.app = app
 	db.init_app(app)
 
@@ -62,5 +51,5 @@ def connect_to_db(app):
 if __name__ == "__main__":
 
 	from server import app
-	connect_to_db(app)
-	print("Connected to database.")
+	connect_to_db(app, 'kindred')
+	print('Connected to database.')
--- seed.py
+++ seed.py
@@ -1,7 +1,7 @@
 """Utility file to seed kindred database from Native-Land data"""
 
 from sqlalchemy import func
-from models import Tribes, Locations, connect_to_db, db 
+from models import Tribe, Language, connect_to_db, db 
 from routes import app
 import json
 
@@ -21,7 +21,7 @@ def load_tribes():
 
 	print('Tribes')
 
-	Tribes.query.delete()
+	Tribe.query.delete()
 
 	tribe_file = "seed_data/indigenousTerritories.json"
 	tribe_dict = json_reader(tribe_file)
@@ -30,20 +30,49 @@ def load_tribes():
 
 	for key in tribe_dict['features']:
 		name = tribe_dict['features'][i]['properties']['Name']
-		# coordinates = tribe_dict['features'][i]['geometry']['coordinates']
+		region = None
+		description = None
 		i += 1
 
-		tribe = Tribes(name=name)
+		tribe = Tribe(name=name,
+					  region=region,
+					  description=description
+					  )
 
 		db.session.add(tribe)
 
 	db.session.commit()
+
+
+def load_languages():
+	"""Load language infor from indigenousLanguages.json into 
+	database."""
 	
+	print('Languages')
+
+	Language.query.delete()
+
+	lang_file = "seed_data/indigenousLanguages.json"
+	lang_dict = json_reader(lang_file)
+
+	i = 0
+
+	for key in lang_dict['features']:
+		language_name = lang_dict['features'][i]['properties']['Name']
+		i += 1
+
+		language = Language(language_name=language_name)
+
+		db.session.add(language)
+
+	db.session.commit()
+
+
 
 def set_val_tribe_id():
 	"""Set value for the next tribe_id after seeding database"""
 
-	result = db.session.query(func.max(Tribes.tribe_id)).one()
+	result = db.session.query(func.max(Tribe.tribe_id)).one()
 	max_id = int(result[0])
 
 	query = "SELECT setval('tribes_tribe_id_seq', :new_id)"
@@ -59,4 +88,5 @@ if __name__ == "__main__":
 
     # Import different types of data
     load_tribes()
-    set_val_user_id()
+    load_languages()
+    set_val_tribe_id()
