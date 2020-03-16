8f94510d6f55c66536a59769b23cc0fee84ed2bf
Briana Smejkal <brianasmejkal@gmail.com>
2020-03-16 16:08:25 +0000

    Updated routes.py and details.html to display information

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
   
 from flask import Flask, render_template
+from models import Tribe
+
 
 app = Flask(__name__)
 
@@ -8,3 +10,13 @@ def index():
 	"""Home page"""
 
 	return render_template('index.html')
+
+
+@app.route('/details/<int:tribe_id>')
+def display_details(tribe_id):
+	"""Tribe details page."""
+
+	tribe = Tribe.query.get(tribe_id)
+
+	return render_template('details.html',
+						   tribe=tribe)
